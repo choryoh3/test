4751a565dac8b193fa55c38a18175126fd15ca13
choryoh3 <choryoh3@yahoo.co.jp>
2026-08-14 21:36:10 +0900

    Revert "Revert "first commit""

--- back/index.ts
+++ back/index.ts
@@ -1,8 +1,14 @@
 type MyType = {
+    sex:number;
+    email:string;
     name: string;
     age: number;
 };
 interface MyInterface11111 {
+    stringProperty: string;
+    numberProperty: number;
+}
+interface MyInterface22222 {
     stringProperty: string;
     numberProperty: number;
 }
